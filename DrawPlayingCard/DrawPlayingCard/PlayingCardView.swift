f5c3f4b6135415d6426da2d89ffeeee455aaee14
sunset wan <sunsetwan@gmail.com>
2019-03-29 14:29:31 +0800

    initialization

---
+++ DrawPlayingCard/DrawPlayingCard/PlayingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardView.swift
+//  DrawPlayingCard
+//
+//  Created by sunsetwan on 2019/3/29.
+//  Copyright © 2019 sunsetwan. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DrawPlayingCard/DrawPlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawPlayingCard
+//
+//  Created by sunsetwan on 2019/3/29.
+//  Copyright © 2019 sunsetwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
